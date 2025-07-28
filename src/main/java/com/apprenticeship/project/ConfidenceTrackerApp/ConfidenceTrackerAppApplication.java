e6cf91157ac2d08ce4139bb5a49d03bebfeed26a
Laila <laila.daher@sainsburys.co.uk>
2025-07-28 16:19:38 +0100

    initialize ConfidenceTrackerApp with Gradle setup and basic Spring Boot configuration

---
+++ src/main/java/com/apprenticeship/project/ConfidenceTrackerApp/ConfidenceTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.apprenticeship.project.ConfidenceTrackerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfidenceTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfidenceTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apprenticeship/project/ConfidenceTrackerApp/ConfidenceTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apprenticeship.project.ConfidenceTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidenceTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
